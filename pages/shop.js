e4e4d1ebeb4dfbd089eb6fd95677c9a193666f38
Md Altaf Hossen <web.altaf.1@gmail.com>
2023-04-16 03:15:05 +0600

    shop page product , sidebar, filter, dynamic data added

--- pages/shop.js
+++ pages/shop.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import Layout from '../components/Layout';
-import { Slider } from '@mui/material';
+import { Rating, Slider } from '@mui/material';
+import Link from 'next/link';
+import Image from 'next/image';
+import { toast } from 'react-hot-toast';
 
-const shop = () => {
+export async function getServerSideProps(context) {
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json();
+    return {
+        props: {
+            data
+        }
+    };
+}
+
+const shop = ({ data }) => {
     const [value, setValue] = useState([0, 150]);
-
+    const brands = data?.products?.map(product => product.brand);
+    const category = data?.products?.map(product => product.category);
     const handleOnChange = (event, newValue) => {
         setValue(newValue);
         if (typeof onChange === 'function') {
@@ -15,11 +29,11 @@ const shop = () => {
     return (
         <Layout>
             <div>
-                <div className="page-content">
+                <div className="shop page-content">
                     <div className="container">
                         <div className="row">
 
-                            <div className="col-lg-9">
+                            <div className="col-lg-10">
                                 <div className="widget widget-clean justify-content-between">
                                     <span>Showing 4 of 4 Products</span>
                                     <div>
@@ -27,119 +41,159 @@ const shop = () => {
 
                                     </div>
                                 </div>
+                                <div className="products">
+                                    <div className="shop-products">
+                                        {
+                                            data?.products?.map((d) => {
+                                                return (
+                                                    <div key={d?.id} className='shop-single-product'>
+                                                        <figure className='product-media'>
+                                                            <span className="product-label label-top">Top</span>
+                                                            <Link style={{ marginTop: '-21px' }} href='/shop'>
+                                                                <div style={{ width: '217px', height: '217px' }}>
+                                                                    <Image
+                                                                        width={217}
+                                                                        height={217}
+                                                                        src={d?.thumbnail}
+                                                                        className=""
+                                                                        alt=""
+
+                                                                    />
+
+                                                                    <button onClick={(e) => {
+                                                                        e.preventDefault();
+                                                                        toast.success('product added in cart');
+
+                                                                    }} className='shop-add-to-cart-button'>Add to Cart<i class="far plus-ico fa-plus-square" aria-hidden="true"></i></button>
+
+
+
+                                                                </div>
+                                                            </Link>
+                                                        </figure>
+                                                        <div className="product-body">
+                                                            <div className="product-cat">
+                                                                <Link href="/shop/?category=fruit">
+                                                                    {d?.category}
+                                                                </Link>
+                                                            </div>
+                                                            <div className="product-title">
+                                                                <Link href="/shop/?category=fruit">
+                                                                    {d?.title}
+                                                                </Link>
+                                                            </div>
+                                                            <div className="product-price d-flex gap-2 justify-content-center">
+                                                                <div className="new-price">
+                                                                    <p>${d?.price}</p>
+                                                                </div>
+                                                                <div className="shop-old-price">
+                                                                    <p>$45</p>
+                                                                </div>
+                                                            </div>
+                                                            <div className="ratings-container mb-3">
+                                                                <div className="ratings">
+                                                                    <Rating name="read-only" value={parseInt(d?.rating)} readOnly />
+                                                                </div>
+                                                                <div className="ratings-texts">
+                                                                    ( 2 Reviews )
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
                             </div>
-                            <aside className='col-lg-3 order-lg-first'>
+                            <aside className='col-lg-2 order-lg-first'>
                                 <div className="sticky-content">
                                     <aside className='sidebar sidebar-shop'>
                                         <div>
                                             <div className="widget  widget-clean">
                                                 <label htmlFor="filter">Filter:</label>
-                                                <a class="sidebar-filter-clear" href="#">Clean All</a>
+                                                <a className="sidebar-filter-clear" href="#">Clean All</a>
                                             </div>
-                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }} className="categories">
-                                                <div className="d-flex justify-content-between">
+                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }} className="shop-categories mb-3 pb-2">
+                                                <div className="d-flex justify-content-between align-items-center">
 
                                                     <h5>Category</h5>
-                                                    <i class="fa fa-caret-down"></i>
+                                                    <i className="fa fa-caret-down"></i>
                                                 </div>
-                                                <li>Furniture</li>
-                                                <li>Coffe</li>
-                                                <li>Lighting</li>
-                                                <li>Decoration</li>
-                                                <li>Electronics</li>
-                                                <li>Sofa & bed</li>
+                                                {
+                                                    category?.map(cat => <li >{cat}</li>)
+                                                }
+                                                
 
                                             </div>
-                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }}>
-                                                <div className="d-flex justify-content-between">
+                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }} className='mb-3 pb-2'>
+                                                <div className="d-flex justify-content-between align-items-center">
 
                                                     <h5>Size</h5>
-                                                    <i class="fa fa-caret-down"></i>
+                                                    <i className="fa fa-caret-down"></i>
                                                 </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="extra-small" />
-                                                    <label class="form-check-label" for="extra-small">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="checkbox" id="extra-small" />
+                                                    <label className="form-check-label" for="extra-small">
                                                         Extra Small
                                                     </label>
                                                 </div>
 
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="small" />
-                                                    <label class="form-check-label" for="small">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="checkbox" id="small" />
+                                                    <label className="form-check-label" for="small">
                                                         Small
                                                     </label>
                                                 </div>
 
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="medium" />
-                                                    <label class="form-check-label" for="medium">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="checkbox" id="medium" />
+                                                    <label className="form-check-label" for="medium">
                                                         Medium
                                                     </label>
                                                 </div>
 
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="large" />
-                                                    <label class="form-check-label" for="large">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="checkbox" id="large" />
+                                                    <label className="form-check-label" for="large">
                                                         Large
                                                     </label>
                                                 </div>
 
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="extra-large" />
-                                                    <label class="form-check-label" for="extra-large">
+                                                <div className="form-check">
+                                                    <input className="form-check-input" type="checkbox" id="extra-large" />
+                                                    <label className="form-check-label" for="extra-large">
                                                         Extra Large
                                                     </label>
                                                 </div>
                                             </div>
-                                            {/* color  */}
-                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }}>
-                                                <div className="d-flex justify-content-between">
+                                            {/* brand  */}
+                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }} className='mb-3 pb-2'>
+                                                <div className="d-flex justify-content-between align-items-center">
 
                                                     <h5>Brand</h5>
-                                                    <i class="fa fa-caret-down"></i>
+                                                    <i className="fa fa-caret-down"></i>
                                                 </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="next-brand" />
-                                                    <label class="form-check-label" for="next-brand">
-                                                        Next
-                                                    </label>
-                                                </div>
+                                                {
+                                                    brands?.map((b) => {
+                                                        return <div className="form-check">
+                                                            <input className="form-check-input" type="checkbox" id="next-brand" />
+                                                            <label className="form-check-label" for="next-brand">
+                                                                {b}
+                                                            </label>
+                                                        </div>
+                                                    })
+                                                }
 
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="nike" />
-                                                    <label class="form-check-label" for="nike">
-                                                        Nike
-                                                    </label>
-                                                </div>
 
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="geox" />
-                                                    <label class="form-check-label" for="geox">
-                                                        Geox
-                                                    </label>
-                                                </div>
-
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="marcel" />
-                                                    <label class="form-check-label" for="marcel">
-                                                        Marcel
-                                                    </label>
-                                                </div>
-
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" id="nokia" />
-                                                    <label class="form-check-label" for="nokia">
-                                                        Nokia
-                                                    </label>
-                                                </div>
                                             </div>
                                             {/* price range  */}
 
-                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }}>
-                                                <div className="d-flex justify-content-between">
+                                            <div style={{ borderBottom: "0.1rem solid #ebebeb" }} className='mb-3 pb-2s'>
+                                                <div className="d-flex justify-content-between align-items-center">
 
                                                     <h5>Price</h5>
-                                                    <i class="fa fa-caret-down"></i>
+                                                    <i className="fa fa-caret-down"></i>
                                                 </div>
                                                 <div>
                                                     <div className="price-range-slider">
